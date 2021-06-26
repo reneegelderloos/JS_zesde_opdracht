79e00e6668b3b0eb5bf5c294b464f564adf1d78a
reneegelderloos <84631679+reneegelderloos@users.noreply.github.com>
2021-06-26 11:33:23 +0200

    Add files via upload

---
+++ zesde_opdracht/script.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+    console.log("Maak het deeg");
+    console.log("Rol het deeg uit");
+    console.log("Versier het deeg met lekkere dingen");
+    console.log("Stop de pizza in de oven");
+};
+
+const makeSushi = function () {
+    console.log("Kook het rijst");
+    console.log("Snij de stukjes vis en groente");
+    console.log("Abracadabra en maak er sushi van");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
